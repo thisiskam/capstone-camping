fa90ab94dc3ed1e99a5adfeea93347d87cf12510
Kameron Pete <kameroncpete@gmail.com>
2024-05-30 16:40:15 +0100

    added dummy data for reviews and titles, mapped it so its layed out relatively well, when item is clicked it navigates to th e single item component, single item component has a go back butten currently to go back to all items

--- src/client/components/CR.jsx
+++ src/client/components/CR.jsx
@@ -4,11 +4,50 @@
 // import Login from "./Login";
 
 //cr means Camp Reviews
+
+
+import { useState , useEffect} from "react";
+import { useNavigate } from "react-router-dom";
+
+
 const CR = () => {
-  console.log("hello world!");
+  const [items, setItems] = useState([])
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    const sampleItems = [
+      {rating:5, title:"camping item 1", id:1},
+      {rating:4, title:"camping item 2" , id:2},
+      {rating:3.2, title:"camping item 3", id:3},
+      {rating:4, title:"camping item 4", id:4},
+      {rating:2, title:"camping item 5", id:5},
+      {rating:4, title:"camping item 6", id:6},
+      {rating:1, title:"camping item 7", id:7},
+      {rating:2, title:"camping item 8", id:8},
+      {rating:3, title:"camping item 9", id:9},
+      {rating:1.4, title:"camping item 10", id:10},
+      {rating:2.6, title:"camping item 11", id:11},
+      {rating:4.3, title:"camping item 12", id:12},
+      {rating:4.2, title:"camping item 13", id:13}  
+    ];
+
+    setItems(sampleItems);
+  }, []); 
+
   return (
     <div className="App">
       <h1>this will be a camp review site</h1>
+      <div className="all_items">
+          {items.map((item)=>{
+              return (
+                  <div key={item.id} className="single-item">
+                      <img src="src/client/assets/star-icon.svg" alt="star" />
+                      <p className="single-item-review">{item.rating}</p>
+                      <p className="single-item-title" onClick={() => navigate(`/cr/${item.id}`)}>{item.title}</p>
+                  </div>
+              )})
+          }
+      </div>
     </div>
   );
 };
--- src/client/components/SingleItem.jsx
+++ src/client/components/SingleItem.jsx
@@ -1,11 +1,14 @@
 // import stuff
 
+import { useNavigate } from "react-router-dom";
+
 // the main page stuff
 export default function SingleItem() {
+  const navigate = useNavigate()
   console.log("hello world!");
   return (
     <>
-      <button>Logout</button>
+      <button onClick={() => navigate('/')}>Back to all items</button>
     </>
   );
 }
