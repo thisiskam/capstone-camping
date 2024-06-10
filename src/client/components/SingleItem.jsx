80b7f5464570e7a7812ed3be88542199f4642e7d
Kameron Pete <kameroncpete@gmail.com>
2024-06-10 17:32:31 +0100

    most information now loads to the page for a non logged in user, the only thing that still needs to load is the comments section

--- src/client/components/SingleItem.jsx
+++ src/client/components/SingleItem.jsx
@@ -1,17 +1,177 @@
 // import stuff
 
 import { useNavigate } from "react-router-dom";
+import { useState , useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useRef } from "react";
 
-// the main page stuff
+
 export default function SingleItem() {
+  const [categories, setCategories] = useState([])
+  const [itemCategory, setItemCategory] = useState()
+  const [itemDetails, setItemDetails] = useState([])
+  const [itemReviews, setItemReviews] = useState([])
+  const [imagePath, setImagePath] = useState('')
+  const [oneStar, setOneStar] = useState(0)
+  const [twoStar, setTwoStar] = useState(0)
+  const [threeStar, setThreeStar] = useState(0)
+  const [fourStar, setFourStar] = useState(0)
+  const [fiveStar, setFiveStar] = useState(0)
+  const [average, setAverage] = useState (0)
+  const [isAdmin, setIsAdmin] = useState(false)
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const { id } = useParams()
   const navigate = useNavigate()
-  console.log("hello world!");
+
+                      //will need to be deleted later after getting the right api path
+                      const [users, setUsers] = useState([])
+
+
+  const sampleCategories =[
+    {id:1,	name: "Backpack"},
+    {id:2, name:	"Tent"},
+    {id:3,	name: "Hiking Boots"},
+    {id:4,	name: "Cookware"},
+    {id:5,	name: "Sleeping Bag"},
+    {id:6,	name: "Water Bottle"}]
+
+  useEffect (() => {
+      async function fetchItemDetails (id) {
+        const response = await fetch("http://localhost:3000/api/items/" + id)
+        const api = await response.json()
+        setItemDetails(api.singleItem[0])
+      }
+      fetchItemDetails(id)
+      
+      async function fetchItemReviews (id) {
+        const response = await fetch("http://localhost:3000/api/items/" + id + "/reviews")
+        const api = await response.json()
+        setItemReviews(api.getReviews)
+      }
+      fetchItemReviews(id)
+      setCategories(sampleCategories)
+
+
+                // will need to be deleted when i get the right api path
+                async function fetchUsers () {
+                  const response = await fetch("http://localhost:3000/api/users/")
+                  const api = await response.json()
+                  setUsers(api.users)
+                }
+                fetchUsers()
+  },[])
+
+  useEffect (() => {
+    async function findCategory () {
+      let res = getCategory()
+      if (res.length > 0 && res[0]) {
+        setItemCategory(res[0].name);
+    }}
+    findCategory()
+  },[itemDetails])
+
+  function getCategory() {
+    return categories.filter(category => category.id === itemDetails.category_id);
+  }
+
+ 
+  useEffect (() => {
+    async function setImage () {
+      if (itemDetails.length !== 0) {
+        setImagePath(itemDetails.imageurl)
+        const key = itemDetails.imageurl.slice(0,2)
+         if (key) {
+          findImage(key)
+        }
+      }
+    }
+    setImage()
+  },[itemDetails])
+  function findImage (key) {
+      if (key === "bp") {
+        setImagePath("/src/client/assets/backpacks/" + itemDetails.imageurl) 
+      }
+      if (key === "bo") {
+        setImagePath("/src/client/assets/boots/" + itemDetails.imageurl) 
+      }
+      if (key === "cw") {
+        setImagePath("/src/client/assets/cookware/" + itemDetails.imageurl) 
+      }
+      if (key === "sb") {
+        setImagePath("/src/client/assets/sleepingBags/" + itemDetails.imageurl) 
+      }
+      if (key === "te") {
+        setImagePath("/src/client/assets/tents/" + itemDetails.imageurl) 
+      }
+      if (key === "wb") {
+        setImagePath("/src/client/assets/boots/" + itemDetails.imageurl) 
+      }
+    }
+  useEffect (() => {
+    async function setStars () {
+      if (itemReviews.length !== 0) {
+        console.log(itemReviews);
+        if (oneStar == 0 && twoStar == 0 && threeStar == 0 && fourStar == 0 && fiveStar == 0) {
+        howManyStars()
+        }
+      }
+    }
+    setStars()
+  },[imagePath, itemCategory, itemDetails])
+
+  function howManyStars() {
+    itemReviews.forEach((review) => {
+      if (review.rating === 5) {
+        setFiveStar(prevCount => prevCount + 1);
+    }
+    if (review.rating === 4) {
+        setFourStar(prevCount => prevCount + 1);
+    }
+    if (review.rating === 3) {
+        setThreeStar(prevCount => prevCount + 1);
+    }
+    if (review.rating === 2) {
+        setTwoStar(prevCount => prevCount + 1);
+    }
+    if (review.rating === 1) {
+        setOneStar(prevCount => prevCount + 1);
+    }
+  })}
+
+// useEffect (() => {
+//   async function b () {
+//     if (users.length !== 0) {
+//     const res = getUserName(3)
+//     console.log(res);
+//   }}
+//   b()
+// },[])
+
+function getUserName (id) {
+    const res = users.find(user => user.id === id)
+    return res.username
+  }
+
+
+
+  useEffect (() => {
+    function reviewAverage () {
+      if (itemReviews.length !== 0) {
+        const sum = itemReviews.reduce((accumulator, currentValue) => {
+          return accumulator + currentValue.rating
+        } , 0 )
+        const average = sum / itemReviews.length
+        setAverage(average)
+      }
+    }
+    reviewAverage()
+  },[itemReviews])
   return (
     <>
       <div className="App">
         <div className="home-content">
-          <div class="left-container" id="left-container-single-item" >
-            <h5 className="item-category"><b>Category:</b> Backpacks</h5>
+          <div className="left-container" id="left-container-single-item" >
+            <h5 className="item-category"><b>CATEGORY:</b>{itemCategory}</h5>
             <div className="review-overview">
               <div className="star-category">
                 <div className="star-box">
@@ -22,7 +182,7 @@ export default function SingleItem() {
                   <img src="/src/client/assets/star-icon.svg" alt="" />
                 </div>
                 <div className="num-reviews">
-                  <h6>12</h6>
+                  <h6>{fiveStar}</h6>
                 </div>
               </div>
               <div className="star-category">
@@ -33,7 +193,7 @@ export default function SingleItem() {
                   <img src="/src/client/assets/star-icon.svg" alt="" />
                 </div>
                 <div className="num-reviews">
-                  <h6>9</h6>
+                  <h6>{fourStar}</h6>
                 </div>
               </div>
               <div className="star-category">
@@ -43,7 +203,7 @@ export default function SingleItem() {
                   <img src="/src/client/assets/star-icon.svg" alt="" />
                 </div>
                 <div className="num-reviews">
-                  <h6>3</h6>
+                  <h6>{threeStar}</h6>
                 </div>
               </div>
               <div className="star-category">
@@ -52,7 +212,7 @@ export default function SingleItem() {
                   <img src="/src/client/assets/star-icon.svg" alt="" />
                 </div>
                 <div className="num-reviews">
-                  <h6>1</h6>
+                  <h6>{twoStar}</h6>
                 </div>
               </div>
               <div className="star-category">
@@ -60,44 +220,48 @@ export default function SingleItem() {
                   <img src="/src/client/assets/star-icon.svg" alt="" />
                 </div>
                 <div className="num-reviews">
-                  <h6>0</h6>
+                  <h6>{oneStar}</h6>
                 </div>
               </div>
             </div>
-            <button onClick={() => navigate('/')}>Back to all items</button>
+            <div className="average">
+              <h5>REVIEW AVERAGE</h5>
+              <div>
+                <img src="/src/client/assets/star-icon.svg" width="30px" alt="" />
+                <h4>{average}</h4>
+              </div>
+            </div>
           </div>
           <div className="center-container" id="center-container-single-item">
-              <h2>Kelty Backpack</h2>
+              <h2>{itemDetails.title}</h2>
               <div className="item-description">
-                <h4>Description</h4>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque tristique tempus mauris, et placerat tellus mattis ac. Duis erat purus, pellentesque in lectus vel, fermentum gravida est. Donec metus est, viverra a tristique rhoncus, porta vel purus. Curabitur commodo aliquet orci, sed vehicula ligula sagittis a. Morbi neque dolor, egestas id tortor ut, rutrum convallis leo. Aliquam erat volutpat. Vestibulum ornare ante quis mauris auctor laoreet. Praesent eget tellus augue. Donec egestas arcu id tortor egestas auctor. Pellentesque a dui nec mi commodo varius. Nullam quis arcu orci. Vivamus luctus odio quis vestibulum iaculis. Aenean eu ornare magna, cursus dapibus.</p>
+                <h4>DESCRIPTION</h4>
+                <p>{itemDetails.description}</p>
               </div>
               <div className="item-reviews">
-                <h4>Reviews</h4>
-                  <div className="review">
-                    <div className="first-line-review">
-                      <h6>username</h6>
-                      <p>4</p>
-                      <img src="/src/client/assets/star-icon.svg" alt="star" className="star-icon-review"/>
-                    </div>
-                    <h5>Review Title</h5>
-                    <p className="review-p">gittis a. Morbi neque dolor, egestas id tortor ut, rutrum convallis leo. Aliquam erat volutpat. Vestibulum ornare ante quis mauris auctor laoreet. Praesent eget tellus augue. Donec egestas arcu id tortor egestas auct</p>
-                    <div className="item-comments">
-                      <h5>Comments</h5>
-                      <div className="comment">
-                        <h6>username</h6>
-                        <p>auctor laoreet. Praesent eget tellus augue. Donec</p>
+                <h4>REVIEWS</h4>
+                  {itemReviews.map((review) => {
+                    return (
+                      <div className="review" key={review.id}>
+                        <div className="first-line-review">
+                          <h6>{getUserName(review.user_id)}</h6>
+                          <p>{review.rating}</p>
+                          <img src="/src/client/assets/star-icon.svg" alt="star" className="star-icon-review"/>
+                        </div>
+                        <p className="review-p">{review.review_text}</p>
+                        <div className="item-comments">
+                          <h5>Comments</h5>
+                          <div className="comment">
+                            <h6>username</h6>
+                            <p>auctor laoreet. Praesent eget tellus augue. Donec</p>
+                          </div>
+                        </div>
                       </div>
-                      <div className="comment">
-                        <h6>username</h6>
-                        <p>t, consectetur adipiscing elit. Quisque tristique tempus mauris, et placerat tellus mattis ac. Duis erat purus, pellentesque in lectus vel, fermentum gravida est. Donec metus est, viverra a tristique rhoncus, porta vel purus. Curabitur com</p>
-                      </div>
-                    </div>
-                  </div>
+                    )})}
               </div>
           </div>
           <div className="right-container">
-            <img src="/src/client/assets/backpacks/bp1.png"/>
+            <img src={imagePath}/>
           </div>
         </div>
       </div>
