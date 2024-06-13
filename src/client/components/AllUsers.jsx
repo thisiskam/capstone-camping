16d9110dacc9af5c0b7ece52053cfcf326e64da8
ArthurYuLunHung <arthur0420@gmail.com>
2024-06-13 09:34:23 -0700

    CSS is working

--- src/client/components/AllUsers.jsx
+++ src/client/components/AllUsers.jsx
@@ -1,9 +1,107 @@
 // import stuff to import
+import "./AllUsers.css";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function AllUsers() {
+  const navigate = useNavigate();
+  const [users, setUsers] = useState();
+  const [isAdmin, setIsAdmin] = useState(false);
+  const token = localStorage.getItem("token");
+
+  // const [searchParams, setSearchParams] = useState("");
+
+  useEffect(() => {
+    async function getAllUsers() {
+      const response = await fetch("http://localhost:3000/api/users");
+      const api = await response.json();
+      // console.log(api.users);
+      setUsers(api.users);
+    }
+    getAllUsers();
+
+    token ? setIsAdmin(true) : setIsAdmin(false);
+  }, []);
+
+  // console.log("users", users);
+  const usersToDisplay = users;
+  // console.log("usersToDisplay", usersToDisplay);
+
   return (
-    <>
-      <h1>Users</h1>
-    </>
+    <div className="App1">
+      <h1 className="h1">USERS</h1>
+      <div className="home-content1">
+        <div className="left-container1">
+          {isAdmin === true ? (
+            <div>
+              <img
+                src="src/client/assets/user-id-svgrepo-com (1).svg"
+                width="30px"
+                alt=""
+              />
+              <button className="selected">USERS</button>
+              <br />
+              <img
+                src="src/client/assets/tent-4-svgrepo-com.svg"
+                width="30px"
+                alt=""
+              />
+              <button
+                onClick={() => {
+                  navigate("/adminitems");
+                }}
+              >
+                ITEMS
+              </button>
+            </div>
+          ) : (
+            <div></div>
+          )}
+        </div>
+        <div className="center-container1">
+          <table>
+            <thead>
+              <tr>
+                <th className="h2">ID</th>
+                <th className="h2">NAME</th>
+                <th className="h2">EMAIL</th>
+                <th className="h2">IS_ADMIN</th>
+              </tr>
+            </thead>
+            <tbody>
+              {usersToDisplay &&
+                usersToDisplay.map((user) => {
+                  return (
+                    <tr key={user.id}>
+                      <td className="h2">{user.id}</td>
+                      <td className="h2">{user.username}</td>
+                      <td className="h2">{user.email}</td>
+                      <td className="h2">{user.is_admin ? "true" : "false"}</td>
+                    </tr>
+                  );
+                })}
+            </tbody>
+          </table>
+        </div>
+
+        <div className="right-container1">
+          {isAdmin === true ? (
+            <div>
+              <button
+                className="green-btn"
+                // onClick={() => {
+                //   navigate("/allusers");
+                // }}
+              >
+                +USER
+              </button>
+              <br />
+            </div>
+          ) : (
+            <div></div>
+          )}
+        </div>
+      </div>
+    </div>
   );
 }
--- src/client/components/CR.jsx
+++ src/client/components/CR.jsx
@@ -5,54 +5,99 @@
 
 //cr means Camp Reviews
 
-
-import { useState , useEffect} from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
-
 const CR = () => {
-  const [items, setItems] = useState([])
-  const navigate = useNavigate()
-  const [searchParams, setSearchParams] = useState("")
-  const [isAdmin, setIsAdmin] = useState(false)
-  const token = localStorage.getItem("token")
+  const [items, setItems] = useState([]);
+  const navigate = useNavigate();
+  const [searchParams, setSearchParams] = useState("");
+  const [isAdmin, setIsAdmin] = useState(false);
+  const token = localStorage.getItem("token");
 
   useEffect(() => {
-
-    async function fetchItems () {
-      const response = await fetch("http://localhost:3000/api/items") 
-      const api = await response.json()
-      setItems(api.items)
+    async function fetchItems() {
+      const response = await fetch("http://localhost:3000/api/items");
+      const api = await response.json();
+      setItems(api.items);
     }
-    fetchItems()
+    fetchItems();
 
-    token ? setIsAdmin(true) :setIsAdmin(false)
-  }, []); 
+    token ? setIsAdmin(true) : setIsAdmin(false);
+  }, []);
 
-
-
-  const displayedItems = !searchParams ? items : items.filter((item) => item.title.toLowerCase().includes(searchParams.toLowerCase()))
+  const displayedItems = !searchParams
+    ? items
+    : items.filter((item) =>
+        item.title.toLowerCase().includes(searchParams.toLowerCase())
+      );
   return (
     <div className="App">
-      <img src="src/client/assets/pexels-bazil-elias-1351340-2612228.jpg" alt="" className="main-img"/>
+      <img
+        src="src/client/assets/pexels-bazil-elias-1351340-2612228.jpg"
+        alt=""
+        className="main-img"
+      />
       <h1 className="main-header">LOCALLY REVIEWED CAMPING ITEMS</h1>
       <div className="home-content">
         <div className="left-container">
           <div className="search-header">SEARCH</div>
-          <input type="text" className="search-input" placeholder="Hydroflask" onChange={(e) => {setSearchParams(e.target.value)}}/>
-          {isAdmin === true ? (<div><img src="src/client/assets/user-id-svgrepo-com (1).svg" width="30px" alt="" /><button onClick={() => {navigate("/allusers")}}>Users</button><br /><img src="src/client/assets/tent-4-svgrepo-com.svg" width="30px" alt="" /><button onClick={() => {navigate("/adminitems")}}>Items</button></div>) : (<div></div>)}
+          <input
+            type="text"
+            className="search-input"
+            placeholder="Hydroflask"
+            onChange={(e) => {
+              setSearchParams(e.target.value);
+            }}
+          />
+          {isAdmin === true ? (
+            <div>
+              <img
+                src="src/client/assets/user-id-svgrepo-com (1).svg"
+                width="30px"
+                alt=""
+              />
+              <button
+                onClick={() => {
+                  navigate("/allusers");
+                }}
+              >
+                Users
+              </button>
+              <br />
+              <img
+                src="src/client/assets/tent-4-svgrepo-com.svg"
+                width="30px"
+                alt=""
+              />
+              <button
+                onClick={() => {
+                  navigate("/adminitems");
+                }}
+              >
+                Items
+              </button>
+            </div>
+          ) : (
+            <div></div>
+          )}
         </div>
         <div className="center-container">
-            <h2 className="items-header">REVIEWED ITEMS</h2>
-            {displayedItems.map((item)=>{
-                return (
-                    <div key={item.id} className="single-item">
-                        <img src="src/client/assets/star-icon.svg" alt="star" />
-                        <p className="single-item-rating">4</p>
-                        <p className="single-item-title" onClick={() => navigate(`/items/${item.id}`)}>{item.title}</p>
-                    </div>
-                )}) 
-            }
+          <h2 className="items-header">REVIEWED ITEMS</h2>
+          {displayedItems.map((item) => {
+            return (
+              <div key={item.id} className="single-item">
+                <img src="src/client/assets/star-icon.svg" alt="star" />
+                <p className="single-item-rating">4</p>
+                <p
+                  className="single-item-title"
+                  onClick={() => navigate(`/items/${item.id}`)}
+                >
+                  {item.title}
+                </p>
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
