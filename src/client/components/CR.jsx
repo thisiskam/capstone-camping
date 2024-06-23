b532132958ea74c660b051a1e8f0742648bb4f2e
Katie B <kblatherwick@productscape.tech>
2024-06-23 10:12:22 -0600

    fixed the issues with the admin users and item buttons and containers in css and put in fontawesome

--- src/client/components/CR.jsx
+++ src/client/components/CR.jsx
@@ -7,15 +7,15 @@
 
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser, faTent } from "@fortawesome/free-solid-svg-icons";
 
 const CR = () => {
-
-
   // state storage
   const [items, setItems] = useState([]);
   const [searchParams, setSearchParams] = useState("");
   const [isAdmin, setIsAdmin] = useState(false);
-  const [reviews, setReviews] = useState([])
+  const [reviews, setReviews] = useState([]);
 
   // variable to use navigate
   const navigate = useNavigate();
@@ -24,69 +24,161 @@ const CR = () => {
   const token = localStorage.getItem("token");
 
   // function should work to fetch the loggen in user and check if they are an admin then store admin to state
-    useEffect(() => {
-      async function fetchUser() {
-        try {
-          const apiResponse = await fetch("/api/users/me", {
-            headers: {
-              "Content-Type": "application/json",
-              Authorization: `Bearer ${token}`,
-            },
-          });
-          const result = await apiResponse.json()
-          setIsAdmin(result.is_admin)
-        } catch (error) {
-          console.error("account me route not worky cuz", error);
-        }
+  useEffect(() => {
+    async function fetchUser() {
+      try {
+        const apiResponse = await fetch("/api/users/me", {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        const result = await apiResponse.json();
+        setIsAdmin(result.is_admin);
+      } catch (error) {
+        console.error("account me route not worky cuz", error);
       }
-      fetchUser();
-    }, []);
-  
+    }
+    fetchUser();
+  }, []);
 
   // function to get reviews
-  useEffect (() => {
+  useEffect(() => {
     // maps over each review and gets all the comments associated with any review on the page. then stores them all in an array in state
     async function getReviews() {
       const promises = items.map(async (item) => {
-        const res = await fetch(`http://localhost:3000/api/items/${item.id}/reviews/`)
-        const api = await res.json()
-        return api.getReviews
-      })
-      const results = await Promise.all(promises)
-      const flattenedResults= results.flat()
-      setReviews(flattenedResults)
+        const res = await fetch(
+          `http://localhost:3000/api/items/${item.id}/reviews/`
+        );
+        const api = await res.json();
+        return api.getReviews;
+      });
+      const results = await Promise.all(promises);
+      const flattenedResults = results.flat();
+      setReviews(flattenedResults);
     }
-    getReviews()
-  },[items])
-
+    getReviews();
+  }, [items]);
 
   // function to get average reviews
-  function getStars (id) {
-
+  function getStars(id) {
     // gets only the reviews that match the passed in item id
-    const itemReviews = reviews.filter((review) => review.item_id === id) 
-    let average = 0
+    const itemReviews = reviews.filter((review) => review.item_id === id);
+    let average = 0;
     if (reviews.length !== 0) {
       const sum = itemReviews.reduce((accumulator, currentValue) => {
-        return accumulator + currentValue.rating
-      } , 0 )
-      average = sum / itemReviews.length
+        return accumulator + currentValue.rating;
+      }, 0);
+      average = sum / itemReviews.length;
     }
 
     // returns num of stars for the number of review, "no reviews" if no reviews
-    if(average <= 1 ) {
-      return <img src="/src/client/assets/star-icon.svg" alt="star" className="star-mainpage"/>
-    } if(average <= 2) {
-      return <><img src="/src/client/assets/star-icon.svg" alt="star" className="star-mainpage"/><img src="/src/client/assets/star-icon.svg" alt="star" className="star-mainpage"/></>
-    } if(average <= 3) {
-      return <><img src="/src/client/assets/star-icon.svg" alt="star" className="star-mainpage"/><img src="/src/client/assets/star-icon.svg" alt="star" className="star-mainpage"/><img src="/src/client/assets/star-icon.svg" alt="star" className="star-mainpage"/></>
-    } if(average <= 4) {
-      return <><img src="/src/client/assets/star-icon.svg" alt="star" className="star-mainpage"/><img src="/src/client/assets/star-icon.svg" alt="star" className="star-mainpage"/><img src="/src/client/assets/star-icon.svg" alt="star" className="star-mainpage"/><img src="/src/client/assets/star-icon.svg" alt="star" className="star-mainpage"/></>
-    } if(average <= 5) {
-      return <><img src="/src/client/assets/star-icon.svg" alt="star" className="star-mainpage"/><img src="/src/client/assets/star-icon.svg" alt="star" className="star-mainpage"/><img src="/src/client/assets/star-icon.svg" alt="star" className="star-mainpage"/><img src="/src/client/assets/star-icon.svg" alt="star" className="star-mainpage"/><img src="/src/client/assets/star-icon.svg" alt="star" className="star-mainpage"/></>
-    } else {return <p className="no_reviews">No Reviews</p>}
+    if (average <= 1) {
+      return (
+        <img
+          src="/src/client/assets/star-icon.svg"
+          alt="star"
+          className="star-mainpage"
+        />
+      );
+    }
+    if (average <= 2) {
+      return (
+        <>
+          <img
+            src="/src/client/assets/star-icon.svg"
+            alt="star"
+            className="star-mainpage"
+          />
+          <img
+            src="/src/client/assets/star-icon.svg"
+            alt="star"
+            className="star-mainpage"
+          />
+        </>
+      );
+    }
+    if (average <= 3) {
+      return (
+        <>
+          <img
+            src="/src/client/assets/star-icon.svg"
+            alt="star"
+            className="star-mainpage"
+          />
+          <img
+            src="/src/client/assets/star-icon.svg"
+            alt="star"
+            className="star-mainpage"
+          />
+          <img
+            src="/src/client/assets/star-icon.svg"
+            alt="star"
+            className="star-mainpage"
+          />
+        </>
+      );
+    }
+    if (average <= 4) {
+      return (
+        <>
+          <img
+            src="/src/client/assets/star-icon.svg"
+            alt="star"
+            className="star-mainpage"
+          />
+          <img
+            src="/src/client/assets/star-icon.svg"
+            alt="star"
+            className="star-mainpage"
+          />
+          <img
+            src="/src/client/assets/star-icon.svg"
+            alt="star"
+            className="star-mainpage"
+          />
+          <img
+            src="/src/client/assets/star-icon.svg"
+            alt="star"
+            className="star-mainpage"
+          />
+        </>
+      );
+    }
+    if (average <= 5) {
+      return (
+        <>
+          <img
+            src="/src/client/assets/star-icon.svg"
+            alt="star"
+            className="star-mainpage"
+          />
+          <img
+            src="/src/client/assets/star-icon.svg"
+            alt="star"
+            className="star-mainpage"
+          />
+          <img
+            src="/src/client/assets/star-icon.svg"
+            alt="star"
+            className="star-mainpage"
+          />
+          <img
+            src="/src/client/assets/star-icon.svg"
+            alt="star"
+            className="star-mainpage"
+          />
+          <img
+            src="/src/client/assets/star-icon.svg"
+            alt="star"
+            className="star-mainpage"
+          />
+        </>
+      );
+    } else {
+      return <p className="no_reviews">No Reviews</p>;
+    }
   }
-
 
   // fetches all items and stores them in state
   useEffect(() => {
@@ -105,10 +197,8 @@ const CR = () => {
         item.title.toLowerCase().includes(searchParams.toLowerCase())
       );
 
-
   return (
     <div className="App">
-
       {/* hero section */}
       <img
         src="src/client/assets/pexels-bazil-elias-1351340-2612228.jpg"
@@ -117,11 +207,9 @@ const CR = () => {
       />
       <h1 className="main-header">LOCALLY REVIEWED CAMPING ITEMS</h1>
       <div className="home-content">
-
         {/* search box */}
         <div className="left-container main-left">
           <div className="search-header">SEARCH</div>
-
           {/* input sets search params to imputed  */}
           <input
             type="text"
@@ -131,40 +219,35 @@ const CR = () => {
               setSearchParams(e.target.value);
             }}
           />
-
-          {/* returns buttons for admin only, for all item page and all users page. navigates admin to those pages */}
-          {isAdmin &&
-            (
-              <div className="admin-btn-box">
-                <div className="admin-user-btn">
-                  <img
-                    src="src/client/assets/user-id-svgrepo-com (1).svg"
-                    width="30px"
-                    alt=""
-                  />
-                  <button
-                    onClick={() => {
-                      navigate("/allusers");
-                    }}
-                  >
-                    Users
-                  </button>
-                </div>
-                <div className="admin-items-btn">
-                  <img
-                    src="src/client/assets/tent-4-svgrepo-com.svg"
-                    width="30px"
-                    alt=""
-                  />
-                  <button
-                    onClick={() => {
-                      navigate("/adminitems");
-                    }}
-                  >
-                    Items
-                  </button>
-                </div>
+          {/* /* returns buttons for admin only, for all item page and all users
+          page. navigates admin to those pages */}
+          {isAdmin && (
+            <div className="admin-btn-box">
+              <div className="admin-user-btn">
+                <button
+                  onClick={() => {
+                    navigate("/allusers");
+                  }}
+                >
+                  <i>
+                    <FontAwesomeIcon icon={faUser} />
+                  </i>
+                  USERS
+                </button>
               </div>
+              <div className="admin-items-btn">
+                <button
+                  onClick={() => {
+                    navigate("/adminitems");
+                  }}
+                >
+                  <i>
+                    <FontAwesomeIcon icon={faTent} />
+                  </i>
+                  ITEMS
+                </button>
+              </div>
+            </div>
           )}
         </div>
         <div className="center-container">
@@ -174,11 +257,8 @@ const CR = () => {
           {displayedItems.map((item) => {
             return (
               <div key={item.id} className="single-item">
-
                 {/* calls function to see what the average review for the item is */}
-                <div className="stars-div-main">
-                  {getStars(item.id)}
-                </div>
+                <div className="stars-div-main">{getStars(item.id)}</div>
                 <p
                   className="single-item-title"
                   onClick={() => navigate(`/items/${item.id}`)}
