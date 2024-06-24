230eb31869b72dfbe42c167ab4a5c83c7b0cd096
Chloe Speshock <cespeshock@gmail.com>
2024-06-24 15:34:08 -0500

    secured all users

--- src/client/components/AllUsers.jsx
+++ src/client/components/AllUsers.jsx
@@ -10,6 +10,7 @@ export default function AllUsers() {
   const [users, setUsers] = useState();
   const [addUser, setAddUser] = useState(false);
   const [isAdmin, setIsAdmin] = useState(false);
+  const [secured, setSecured] = useState(false);
   const token = localStorage.getItem("token");
 
   useEffect(() => {
@@ -20,7 +21,25 @@ export default function AllUsers() {
       setUsers(api.users);
     }
     getAllUsers();
-    token ? setIsAdmin(true) : setIsAdmin(false);
+  }, []);
+
+  useEffect(() => {
+    async function fetchUser() {
+      try {
+        const apiResponse = await fetch("/api/users/me", {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        const result = await apiResponse.json();
+        console.log(result);
+        setSecured(result.is_admin);
+      } catch (error) {
+        console.error("account me route not worky cuz", error);
+      }
+    }
+    fetchUser();
   }, []);
 
   // console.log("users", users);
@@ -28,86 +47,91 @@ export default function AllUsers() {
   // console.log("usersToDisplay", usersToDisplay);
 
   return (
-    <div className="App1">
-      <h1 className="h1">USERS</h1>
-      <div className="home-content1">
-        <div className="left-container1">
-          {isAdmin && (
-            <div className="admin-btn-box">
-              <div className="admin-user-btn">
-                <button
-                  onClick={() => {
-                    navigate("/allusers");
-                  }}
-                >
-                  <i>
-                    <FontAwesomeIcon icon={faUser} />
-                  </i>
-                  USERS
-                </button>
-              </div>
-              <div className="admin-items-btn">
-                <button
-                  onClick={() => {
-                    navigate("/adminitems");
-                  }}
-                >
-                  <i>
-                    <FontAwesomeIcon icon={faTent} />
-                  </i>
-                  ITEMS
-                </button>
-              </div>
+    <>
+      {secured && (
+        <div className="App1">
+          <h1 className="h1">USERS</h1>
+          <div className="home-content1">
+            <div className="left-container1">
+              {isAdmin && (
+                <div className="admin-btn-box">
+                  <div className="admin-user-btn">
+                    <button
+                      onClick={() => {
+                        navigate("/allusers");
+                      }}
+                    >
+                      <i>
+                        <FontAwesomeIcon icon={faUser} />
+                      </i>
+                      USERS
+                    </button>
+                  </div>
+                  <div className="admin-items-btn">
+                    <button
+                      onClick={() => {
+                        navigate("/adminitems");
+                      }}
+                    >
+                      <i>
+                        <FontAwesomeIcon icon={faTent} />
+                      </i>
+                      ITEMS
+                    </button>
+                  </div>
+                </div>
+              )}
             </div>
-          )}
-        </div>
-        <div className="center-container1">
-          <table>
-            <thead>
-              <tr>
-                <th className="h2">ID</th>
-                <th className="h2">NAME</th>
-                <th className="h2">EMAIL</th>
-                <th className="h2">IS_ADMIN</th>
-              </tr>
-            </thead>
-            <tbody>
-              {usersToDisplay &&
-                usersToDisplay.map((user) => {
-                  return (
-                    <tr key={user.id}>
-                      <td className="h2">{user.id}</td>
-                      <td
-                        className="h2"
-                        onClick={() => navigate(`/allusers/${user.id}`)}
-                      >
-                        {user.username}
-                      </td>
-                      <td className="h2">{user.email}</td>
-                      <td className="h2">{user.is_admin ? "YES" : "NO"}</td>
-                    </tr>
-                  );
-                })}
-            </tbody>
-          </table>
-        </div>
-        <div className="right-container1">
-          {isAdmin === true ? (
-            <div>
-              <button
-                className="green-btn"
-                onClick={() => {
-                  navigate("/allusers/adduser");
-                }}
-              >
-                +USER
-              </button>
+            <div className="center-container1">
+              <table>
+                <thead>
+                  <tr>
+                    <th className="h2">ID</th>
+                    <th className="h2">NAME</th>
+                    <th className="h2">EMAIL</th>
+                    <th className="h2">IS_ADMIN</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {usersToDisplay &&
+                    usersToDisplay.map((user) => {
+                      return (
+                        <tr key={user.id}>
+                          <td className="h2">{user.id}</td>
+                          <td
+                            className="h2"
+                            onClick={() => navigate(`/allusers/${user.id}`)}
+                          >
+                            {user.username}
+                          </td>
+                          <td className="h2">{user.email}</td>
+                          <td className="h2">{user.is_admin ? "YES" : "NO"}</td>
+                        </tr>
+                      );
+                    })}
+                </tbody>
+              </table>
             </div>
-          ) : (
-            <div></div>
-          )}
+            <div className="right-container1">
+              {isAdmin === true ? (
+                <div>
+                  <button
+                    className="green-btn"
+                    onClick={() => {
+                      navigate("/allusers/adduser");
+                    }}
+                  >
+                    +USER
+                  </button>
+                </div>
+              ) : (
+                <div></div>
+              )}
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      )}
+      {!secured && <p>Not Authorized</p>}
+    </>
   );
 }
